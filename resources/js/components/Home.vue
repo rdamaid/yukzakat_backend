fa282074b7f7efc0922a2190cda4f7702eeb1f4b
mgunturgnwn <muhamadgunturg@gmail.com>
2020-11-11 16:49:16 +0700

    [Guntur] Routing with VueJS! Yey!

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>COBAAA IHHH NYOBAAA :(</p>
+    <p>KOK bootstrapnya gak ke load :(</p>
+  </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any','.*');
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
